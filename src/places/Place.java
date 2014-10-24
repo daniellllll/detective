036b423ba68b98ce8db3f183befc24fa53b483f1
daniel <daniel@daniel>
2014-10-24 20:10:56 +0200

    Items zu Place hinzugefügt

--- src/places/Place.java
+++ src/places/Place.java
@@ -6,26 +6,36 @@ import java.util.List;
 import persons.Person;
 import ui.UI;
 import interfaces.Inspectable;
+import items.Item;
 
 public class Place implements Inspectable {
 
 	private String name;
 	private List<Person> persons;
+	private List<Item> items;
 
 	public Place(String name) {
 		this.name = name;
 		persons = new ArrayList<>();
+		items = new ArrayList<>();
 	}
-	
-	public void addPerson(Person person){
+
+	public void addPerson(Person person) {
 		persons.add(person);
 	}
-	
-	public Inspectable[] getInspectables(){
-		Inspectable insps[] = new Inspectable[persons.size()];
-		int i=0;
-		for(Person p : persons){
-			insps[i++]=(Inspectable)p;
+
+	public void addItem(Item item) {
+		items.add(item);
+	}
+
+	public Inspectable[] getInspectables() {
+		Inspectable insps[] = new Inspectable[persons.size() + items.size()];
+		int i = 0;
+		for (Person p : persons) {
+			insps[i++] = (Inspectable) p;
+		}
+		for (Item item : items) {
+			insps[i++] = (Inspectable) item;
 		}
 		return insps;
 	}
@@ -38,7 +48,7 @@ public class Place implements Inspectable {
 	@Override
 	public void inspect() {
 		UI.write("Following people are in this room:");
-		for(Person p : persons){
+		for (Person p : persons) {
 			UI.write(p.getName());
 		}
 	}
