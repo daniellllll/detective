bc1584af86855dd6f6d814563cdd0b6b2236d356
igelchen <killerpilze.funghi@web.de>
2014-10-26 22:20:36 +0100

    Japanische Namen hinzgugefügt, Methode getFamily entfernt

--- src/persons/Generator.java
+++ src/persons/Generator.java
@@ -4,14 +4,51 @@ import utils.Random;
 import persons.Person.Gender;
 
 public class Generator {
-	
-	private static String[] surnames_m_german = {"Peter", "Michael","Klaus", "Kurt", "Richard", "Franz", "Maximillian", "Johann", "Christian", "Manuel", "Gabriel", "Gerhard", "Paul", "Daniel", "Oskar", "Herbert", "Adolf", "Friedolin", "Janosch", "Jonas", "Fabian", "Philipp", "Sebastian", "Lukas", "Niko", "Olaf"};
-	private static String[] surnames_f_german = {"Anna", "Lisa", "Lena", "Ronja", "Lara", "Maria", "Johanna", "Karolin", "Annemarie", "Lotte", "Lieschen", "Lieselotte", "Laura", "Rebecka", "Alexandra", "Petra", "Beate", "Ursula", "Rosi", "Erika", "Jolanda", "Tina", "Maresa", "Elisabeth", "Josephine", "Heidi"};
-	private static String[] lastnames_german = {"Meyer", "Becker", "Kirschenbaum", "Silbereisen", "Berg", "Stein", "Vogel", "Eckel", "Bamberger", "Hertz", "Bauer", "Henkel", "Vetter", "Hofmann", "Theis", "Schneider", "Fischer", "Weber", "Wagner", "Schulz", "Koch", "Schwarz", "Weiss", "Wolf", "Braun", "Zimmermann", "Schmidt", "Hahn", "Berger"};
-	private static String[] surnames_m_english = {"Zachory", "Mike", "Alan", "Dorian", "Matt", "Douglas", "John", "Jack", "Ronan", "Harry", "Jim", "Ted", "Stewart", "Peter", "Mikel", "Ethan", "Berry", "Phil", "Barney", "George", "Malcolm", "Steven", "Coby", "Hall", "Jason", "Justin", "Charlie", "Joe"}; 
-	private static String[] surnames_f_english = {"Debbie", "Liz", "Lesley", "Kendra", "Jules", "Ruby", "Jean", "Victoria", "Rose", "Maggy", "Diana", "Linda", "Lilith", "Nicole", "Stephanie", "Joana", "Elizabeth", "Megan", "Carry"};
-	private static String[] lastnames_english = {"Smith", "Miller", "Roberts", "Cooper", "Colt", "Middleton", "Tannenbaum", "Kirschenbaum", "Tanner", "Jackson", "Bolton", "King", "Nichols"};
-	private static String[][][] countries = {{surnames_m_german, surnames_f_german, lastnames_german}, {surnames_m_english, surnames_f_english, lastnames_english}};
+
+	private static String[] surnames_m_german = { "Peter", "Michael", "Klaus",
+			"Kurt", "Richard", "Franz", "Maximillian", "Johann", "Christian",
+			"Manuel", "Gabriel", "Gerhard", "Paul", "Daniel", "Oskar",
+			"Herbert", "Adolf", "Friedolin", "Janosch", "Jonas", "Fabian",
+			"Philipp", "Sebastian", "Lukas", "Niko", "Olaf" };
+	private static String[] surnames_f_german = { "Anna", "Lisa", "Lena",
+			"Ronja", "Lara", "Maria", "Johanna", "Karolin", "Annemarie",
+			"Lotte", "Lieschen", "Lieselotte", "Laura", "Rebecka", "Alexandra",
+			"Petra", "Beate", "Ursula", "Rosi", "Erika", "Jolanda", "Tina",
+			"Maresa", "Elisabeth", "Josephine", "Heidi" };
+	private static String[] lastnames_german = { "Meyer", "Becker",
+			"Kirschenbaum", "Silbereisen", "Berg", "Stein", "Vogel", "Eckel",
+			"Bamberger", "Hertz", "Bauer", "Henkel", "Vetter", "Hofmann",
+			"Theis", "Schneider", "Fischer", "Weber", "Wagner", "Schulz",
+			"Koch", "Schwarz", "Weiss", "Wolf", "Braun", "Zimmermann",
+			"Schmidt", "Hahn", "Berger" };
+	private static String[] surnames_m_english = { "Zachory", "Mike", "Alan",
+			"Dorian", "Matt", "Douglas", "John", "Jack", "Ronan", "Harry",
+			"Jim", "Ted", "Stewart", "Peter", "Mikel", "Ethan", "Berry",
+			"Phil", "Barney", "George", "Malcolm", "Steven", "Coby", "Hall",
+			"Jason", "Justin", "Charlie", "Joe" };
+	private static String[] surnames_f_english = { "Debbie", "Liz", "Lesley",
+			"Kendra", "Jules", "Ruby", "Jean", "Victoria", "Rose", "Maggy",
+			"Diana", "Linda", "Lilith", "Nicole", "Stephanie", "Joana",
+			"Elizabeth", "Megan", "Carry" };
+	private static String[] lastnames_english = { "Smith", "Miller", "Roberts",
+			"Cooper", "Colt", "Middleton", "Tannenbaum", "Kirschenbaum",
+			"Tanner", "Jackson", "Bolton", "King", "Nichols" };
+	private static String[] surnames_m_japanese = { "Akio", "Daiki", "Daisuke",
+			"Haruto", "Hayato", "Hiroki", "Hiroshi", "Itsuki", "Kaito",
+			"Masahiro", "Masaru", "Shinichi", "Shin", "Takeshi", "Tetsuya",
+			"Tsubasa", "Yamato", "Yoshio" };
+	private static String[] surnames_f_japanese = { "Ai", "Aiko", "Aimi",
+			"Akemi", "Akira", "Amaya", "Emi", "Beniko", "Chiyoko", "Halura",
+			"Fumiko", "Hina", "Hotaru", "Kazumi", "Mana", "Mayumi", "Mei",
+			"Misaki", "Miyu", "Sakura", "Sayuri", "Teiko", "Yoko", "Yukiko",
+			"Yumi", };
+	private static String[] lastnames_japanese = { "Fujimoto", "Gasai",
+			"Ichigawa", "Ishida", "Joshura", "Kawazu", "Kitamura", "Nishimoto",
+			"Okamura", "Suzuki", "Tanaka", "Tsung", "Yamamoto", "Watabe" };
+	private static String[][][] countries = {
+			{ surnames_m_japanese, surnames_f_japanese, lastnames_japanese },
+			{ surnames_m_german, surnames_f_german, lastnames_german },
+			{ surnames_m_english, surnames_f_english, lastnames_english } };
 	private static String[] surnames_m;
 	private static String[] surnames_f;
 	private static String[] lastnames;
@@ -23,7 +60,7 @@ public class Generator {
 		surnames_m = country[0];
 		surnames_f = country[1];
 		lastnames = country[2];
-		
+
 		Person[] persons = new Person[50];
 
 		int firstGenNum = 20;
@@ -82,14 +119,6 @@ public class Generator {
 			}
 		}
 		return false;
-	}
-
-	private static String getNewFamilyname(Person[] persons) {
-		String lastname = lastnames[Random.randInt(0, lastnames.length)];
-		while (nameExists(persons, lastname)) {
-			lastname = lastnames[Random.randInt(0, lastnames.length)];
-		}
-		return lastname;
 	}
 
 	private static String getSurname(Person[] persons, String lastname,
