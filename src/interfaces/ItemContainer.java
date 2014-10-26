35f83a23dc34e6ba19efeb458b821ec383972bb2
daniellllll <danielnassauer@googlemail.com>
2014-10-26 21:30:55 +0100

    ItemContainer hinzugefügt

---
+++ src/interfaces/ItemContainer.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import items.Item;
+
+public interface ItemContainer {
+
+	public void insertItem(Item item);
+	
+	public void removeItem(Item item);
+
+}
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,15 +1,35 @@
 package player;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import interfaces.ItemContainer;
+import items.Item;
 import places.Place;
 
-public class Player {
+public class Player implements ItemContainer {
 	private Place place;
+	private List<Item> items;
+
+	public Player() {
+		items = new ArrayList<>();
+	}
 
 	public void goTo(Place place) {
 		this.place = place;
 	}
-	
-	public Place getPlace(){
+
+	public Place getPlace() {
 		return place;
+	}
+
+	@Override
+	public void insertItem(Item item) {
+		items.add(item);
+	}
+
+	@Override
+	public void removeItem(Item item) {
+		items.remove(item);
 	}
 }
