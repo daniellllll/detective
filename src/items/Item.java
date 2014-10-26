49e28c0072b03b812f71369db93c4f7fc10ad5c2
daniellllll <danielnassauer@googlemail.com>
2014-10-26 22:39:57 +0100

    Items entfernen sich automatisch aus ItemContainern

--- src/items/Item.java
+++ src/items/Item.java
@@ -4,20 +4,26 @@ import interfaces.Inspectable;
 import interfaces.ItemContainer;
 import interfaces.Useable;
 
-public abstract class Item implements Inspectable, Useable{
+public abstract class Item implements Inspectable, Useable {
 	protected String name;
-	
-	public Item(String name){
+	protected ItemContainer container;
+
+	public Item(String name) {
 		this.name = name;
+		container = null;
 	}
-	
+
 	@Override
 	public String getName() {
 		return name;
 	}
-	
-	public void insertInto(ItemContainer container){
+
+	public void insertInto(ItemContainer container) {
+		if (this.container != null) {
+			this.container.removeItem(this);
+		}
 		container.insertItem(this);
+		this.container = container;
 	}
-	
+
 }
